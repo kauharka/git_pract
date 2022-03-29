808568dd54639e027ceed4d7f0ab61e10b2d2127
rahuak <uranbekovnaaa@gmail.com>
2022-03-29 12:53:08 +0600

    INITIAL COMMIT.

---
+++ git_commands.py
@@ -0,0 +1,13 @@
+git init - .git(скрытая папка)
+git remote add name url -добовляет удалённый сервер
+git pull origin master - стягиваем все изменения
+git status
+git add .
+git commit -m "comment"
+git branch 
+git branch name branch
+git checkout name branch
+git push name branch
+git push origin master
+git reset filename 
+
